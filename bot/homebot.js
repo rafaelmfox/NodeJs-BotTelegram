4784f385f3c61992db9e91585e63ca6079a3d684
Rafael <rafaelmfox@gmail.com>
2020-08-27 05:04:34 -0300

    TCC Bot
    adicionado perghuntas ao json

--- bot/homebot.js
+++ bot/homebot.js
@@ -1,10 +1,34 @@
 const env = require('../config/.env')
 const Telegraf = require('telegraf')
+const Markup = require('telegraf/markup')
 const bot = new Telegraf(env.token)
+var json = require('../json/perguntas.json');
 
-bot.start(ctx => {
-    console.log("abc")
-    ctx.reply("se ta lendo isso meu amigo ta funfando")
+
+//teclado de perguntas
+const tecladoGeral = Markup.keyboard([
+    ['O que é o Vestibulinho?','vai ter prova?'],
+    ['O Vestibulinho oferece vagas para quais cursos? '], 
+]).resize().extra()
+
+
+bot.start((ctx) => {
+    const primeiroNomeUsuario = ctx.from.first_name
+    ctx.reply('Oi, Seja bem vindo: '+primeiroNomeUsuario)
+    ctx.reply('eu sou o seu bot feito para te ajudar \n como posso estar te ajudando?',tecladoGeral )
 })
+
+json.forEach(pergunta => {
+   // console.log(pergunta.pergunta)
+    bot.hears(pergunta.pergunta,  ctx=> {
+        ctx.reply(pergunta.resposta)
+    })
+})
+
+
+
+
+
+
 
 bot.startPolling()
