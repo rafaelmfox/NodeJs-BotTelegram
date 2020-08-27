3f9c05225d0c4c8b2d42b37ceeccbd52f0ee8b6b
Rafael <rafaelmfox@gmail.com>
2020-08-27 05:39:36 -0300

    tcc bot
    adicionado teclado

--- bot/homebot.js
+++ bot/homebot.js
@@ -6,29 +6,47 @@ var json = require('../json/perguntas.json');
 
 
 //teclado de perguntas
-const tecladoGeral = Markup.keyboard([
-    ['O que é o Vestibulinho?','vai ter prova?'],
-    ['O Vestibulinho oferece vagas para quais cursos? '], 
+const teclado1 = Markup.keyboard([
+    ['o que é o vestibulinho?','vai ter prova?'],
+    ['o vestibulinho oferece vagas para quais cursos? '],
+    ['como faço a inscrição?'],['Pagina 2'] 
+]).resize().extra()
+const teclado2 = Markup.keyboard([
+    ['tem idade mínima para fazer os cursos técnicos?'],
+    ['quando começam as inscrições para o vestibulinho? '], 
+    ['ate quando posso me inscrever?'],['Pagina 3'] 
+]).resize().extra()
+const teclado3 = Markup.keyboard([
+    ['quais documentos de identificação preciso para me inscrever?'],
+    ['quando será divulgada a lista de classificação geral do vestibulinho? '], 
+    ['Pagina 1'] 
 ]).resize().extra()
 
 
 bot.start((ctx) => {
     const primeiroNomeUsuario = ctx.from.first_name
     ctx.reply('Oi, Seja bem vindo: '+primeiroNomeUsuario)
-    ctx.reply('eu sou o seu bot feito para te ajudar \n como posso estar te ajudando?',tecladoGeral )
+    ctx.reply('eu sou o seu bot \n como posso estar te ajudando?',teclado1 )
 })
+
+
+bot.hears('Pagina 1',  ctx=> {
+    ctx.reply('como posso estar te ajudando?',teclado1)
+})
+bot.hears('Pagina 2',  ctx=> {
+    ctx.reply('como posso estar te ajudando?',teclado2)
+})
+bot.hears('Pagina 3',  ctx=> {
+    ctx.reply('como posso estar te ajudando?',teclado3)
+})
+
+
 
 json.forEach(pergunta => {
-   // console.log(pergunta.pergunta)
-    bot.hears(pergunta.pergunta,  ctx=> {
+    bot.hears(pergunta.pergunta.toLowerCase(),  ctx=> {
         ctx.reply(pergunta.resposta)
-    })
+    })    
 })
-
-
-
-
-
 
 
 bot.startPolling()
